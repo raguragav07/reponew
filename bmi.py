1c2100fd72ebc68f0510ec8c701ee1d13be5ced0
raguragav07 <raguprabhu434@gmail.com>
2025-06-18 13:43:44 +0530

    calculator

--- bmi.py
+++ bmi.py
@@ -1,13 +1,3 @@
-# Problem: Input weight (kg) and height (m), calculate BMI and print the category:
-
-# Underweight: < 18.5
-
-# Normal: 18.5–24.9
-
-# Overweight: 25–29.9
-
-# Obese: 30+
-
 weight =int(input("enter the weight"))
 height =int(input("enter the height"))
 bmi =weight /(height**2)
